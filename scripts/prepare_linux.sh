9e9c1518d900d40496571c3e14666775a57016f3
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2023-08-25 22:50:27 +0900

    Add wheel build workflow (#1)
    - add working ci build configuration

--- scripts/prepare_linux.sh
+++ scripts/prepare_linux.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-FREETYPE_VERSION="VER-2-13-0"
-FONTCONFIG_VERSION="2.14.0"
+set -eux
+
+FREETYPE_VERSION=${FREETYPE_VERSION:-"VER-2-13-0"}
+FONTCONFIG_VERSION=${FONTCONFIG_VERSION:-"2.14.0"}
 
 install_freetype() {
     git clone https://gitlab.freedesktop.org/freetype/freetype.git \
@@ -12,16 +14,28 @@ install_freetype() {
     make
     make -j
     make install
+    cd ..
 }
 
 install_fontconfig() {
     git clone https://gitlab.freedesktop.org/fontconfig/fontconfig.git \
         -b $FONTCONFIG_VERSION --depth 1
+    cd fontconfig
     ./autogen.sh --sysconfdir=/etc --prefix=/usr --mandir=/usr/share/man
     make -j
     make install
+    cd ..
 }
 
-yum install -y gperf gettext-devel libuuid-devel
+if command -v yum &> /dev/null; then
+    yum --disablerepo=epel install -y gperf gettext-devel libuuid-devel
+elif command -v apk &> /dev/null; then
+    apk add gperf gettext-dev util-linux-dev
+elif command -v apt &> /dev/null; then
+    apt install gperf gettext uuid-dev
+else
+    exit 1
+fi
+
 install_freetype
 install_fontconfig
