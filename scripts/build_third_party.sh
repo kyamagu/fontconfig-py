74f91103e6b0ab1183fa819c6e0c6db803ca9ad5
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2023-08-26 23:11:34 +0900

    use submodules

--- scripts/build_third_party.sh
+++ scripts/build_third_party.sh
@@ -5,37 +5,36 @@ set -eux
 FREETYPE_VERSION=${FREETYPE_VERSION:-"VER-2-13-0"}
 FONTCONFIG_VERSION=${FONTCONFIG_VERSION:-"2.14.0"}
 
-install_freetype() {
-    git clone https://gitlab.freedesktop.org/freetype/freetype.git \
-        -b $FREETYPE_VERSION --depth 1
-    cd freetype
+build_freetype() {
+    cd third_party/freetype
     ./autogen.sh
-    ./configure --sysconfdir=/etc --prefix=/usr --mandir=/usr/share/man
+    ./configure
     make
     make -j
     make install
-    cd ..
+    cd ../..
 }
 
-install_fontconfig() {
-    git clone https://gitlab.freedesktop.org/fontconfig/fontconfig.git \
-        -b $FONTCONFIG_VERSION --depth 1
-    cd fontconfig
-    ./autogen.sh --sysconfdir=/etc --prefix=/usr --mandir=/usr/share/man
+build_fontconfig() {
+    cd third_party/fontconfig
+    ./autogen.sh
     make -j
     make install
-    cd ..
+    cd ../..
 }
 
+# Install build dependencies
 if command -v yum &> /dev/null; then
     yum --disablerepo=epel install -y gperf gettext-devel libuuid-devel
 elif command -v apk &> /dev/null; then
     apk add gperf gettext-dev util-linux-dev
 elif command -v apt &> /dev/null; then
     apt install gperf gettext uuid-dev
+elif command -v brew &> /dev/null; then
+    brew install gperftools gettext automake
 else
     exit 1
 fi
 
-install_freetype
-install_fontconfig
+build_freetype
+build_fontconfig
