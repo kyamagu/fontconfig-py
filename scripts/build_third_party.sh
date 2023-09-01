42e41c6111309a3e2566b647bdf759044998a069
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2023-09-01 14:17:14 +0900

    bundle shared lib

--- scripts/build_third_party.sh
+++ scripts/build_third_party.sh
@@ -2,12 +2,10 @@
 
 set -ex
 
-export CFLAGS="$CFLAGS -fPIC"
-
 build_freetype() {
     cd third_party/freetype
     ./autogen.sh
-    ./configure --disable-shared --without-png --without-harfbuzz --without-brotli --without-librsvg
+    ./configure --without-png --without-harfbuzz --without-brotli --without-librsvg
     make -j
     make install
     cd ../..
@@ -15,7 +13,7 @@ build_freetype() {
 
 build_fontconfig() {
     cd third_party/fontconfig
-    ./autogen.sh --disable-shared --disable-libxml2 --disable-iconv --disable-nls
+    ./autogen.sh --disable-libxml2 --disable-iconv --disable-nls
     make -j
     make install
     cd ../..
