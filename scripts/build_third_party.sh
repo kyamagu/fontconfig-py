509d9217df0c147af23d489e87f8c580642be1bb
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2023-09-01 13:37:44 +0900

    update build options

--- scripts/build_third_party.sh
+++ scripts/build_third_party.sh
@@ -5,7 +5,7 @@ set -eux
 build_freetype() {
     cd third_party/freetype
     ./autogen.sh
-    ./configure --static --without-png --without-harfbuzz --without-brotli --without-librsvg
+    ./configure --disable-shared --without-png --without-harfbuzz --without-brotli --without-librsvg
     make
     make -j
     make install
@@ -14,7 +14,7 @@ build_freetype() {
 
 build_fontconfig() {
     cd third_party/fontconfig
-    ./autogen.sh --static --disable-libxml2 --disable-iconv --disable-nls
+    ./autogen.sh --disable-shared --disable-libxml2 --disable-iconv --disable-nls
     make -j
     make install
     cd ../..
