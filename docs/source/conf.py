a9665648023e03443a3e04042f9c9dc85069ce9b
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-08-15 10:22:56 +0900

    Migrate poetry to uv and fix build issues

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "fontconfig-py"
 copyright = "2023, Kota Yamaguchi"
 author = "Kota Yamaguchi"
-release = "0.1.2"
+release = "0.1.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
